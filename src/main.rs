a64c60b5954572a294b69c693e7a05315fae00d5
brayckner <brayckner@gmail.com>
2024-09-01 22:58:54 -0600

    Adding crate Local to get timestamp functionality and set up root and echo paths.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use chrono::Local;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello from the base path of Fun Functions")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    let current_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    let response_string = format!("[{}] - Welcome to fun functions: {}", current_time, req_body);
+    HttpResponse::Ok().body(response_string)
+}
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
-    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok))).workers(10)
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(echo)
+            .service(hello)
+            .route("/", web::get().to(HttpResponse::Ok))
+    })
+    .workers(10)
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
